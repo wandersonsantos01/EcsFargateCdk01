1f15bd35f5aee1a1fcf752b48d4376dc721e997d
wandersonsantos01 <~wanderson.santos01@gmail.com>
2024-09-30 11:59:56 -0300

    First commit

---
+++ src/main/java/br/com/binsolution/ecs_fargate_cdk_01/EcsFargateCdk01Application.java
@@ -0,0 +1,13 @@
+package br.com.binsolution.ecs_fargate_cdk_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsFargateCdk01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsFargateCdk01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/binsolution/ecs_fargate_cdk_01/EcsFargateCdk01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.binsolution.ecs_fargate_cdk_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsFargateCdk01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
